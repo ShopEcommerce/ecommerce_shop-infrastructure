9efa704ae8c63d4d74e93a91608958879ab34737
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2026-05-01 20:08:53 +0700

    feat: setup base infrastructure for microservices

---
+++ postgres/init-db.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE auth_db;
+CREATE DATABASE account_db;
+CREATE DATABASE catalog_db;
+CREATE DATABASE cart_db;
+CREATE DATABASE order_db;
+CREATE DATABASE payment_db;
+CREATE DATABASE report_db;
+CREATE DATABASE notification_db;
